56aae4e176c05ec73c483d5f2717a9dc79263078
zhazgul <zh.zhoroeva@gmail.com>
2023-01-05 14:55:58 +0600

    createModelsClassDriver andCreateMethodsService

--- src/service/DriverService.java
+++ src/service/DriverService.java
@@ -1,4 +1,24 @@
 package service;
 
+import classees.Driver;
+import classees.License;
+
+import java.util.List;
+
 public interface DriverService {
+    Driver add(Driver driver);
+
+    List<Driver> add(List<Driver> drivers);
+
+    Driver findById(Long id);
+
+    List<Driver> findByName(String name);
+
+    String assignTaxiToDriver(String taxiName, Long driverId);
+
+    String changeTaxiOrDriver(Long driverId, Long taxiId);
+
+    List<Driver> getDriverByTaxiModel(String model);
+
+    void update(String driverName);
 }
