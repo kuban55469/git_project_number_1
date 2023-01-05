27326115167b9251a4687af62ec1c1ca681803f4
Kuban <kubanmuit@gmail.com>
2023-01-05 19:02:48 +0600

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         List<Client> clients = new ArrayList<>(List.of(
-                new Client(1L,"Kuban Kelsinbekov", LocalDate.of(2002,12,10),"+996708281398", BigDecimal.valueOf(20000)),
+                new Client(1L,"Kuban Kelsinbekov", LocalDate.of(2002,12,10),"996708281398", BigDecimal.valueOf(20000)),
                 new Client(2L,"Ilyaz Sariev", LocalDate.of(1990,12,28),"+996222888999", BigDecimal.valueOf(30000)),
                 new Client(3L,"Zhazgul Asanov", LocalDate.of(2003,9,11),"+996333444555", BigDecimal.valueOf(40000))
         ));
