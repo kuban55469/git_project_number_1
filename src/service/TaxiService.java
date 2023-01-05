c1d47d82bc320e8d1a17645ed5cc5128b38cc3c9
SarievIlyaz <ilyaz281290@gmail.com>
2023-01-05 11:24:30 +0300

    creat methods Taxi

--- src/service/TaxiService.java
+++ src/service/TaxiService.java
@@ -1,4 +1,22 @@
 package service;
 
+import classees.Taxi;
+import enums.TaxiType;
+
+import java.util.List;
+import java.util.Map;
+
 public interface TaxiService {
+
+    StringBuilder add(Taxi taxi);
+
+    StringBuilder add(List<Taxi> taxis);
+
+    List<Taxi> findByInitialLetter(String model);
+
+    Map<TaxiType, List<Taxi>> grouping();
+
+    List<Taxi> filterByTaxiType(String taxiType);
+
+    void update(Long id);
 }
